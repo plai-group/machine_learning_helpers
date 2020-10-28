987e0e17d63f111a382ca67ddfd2b77eb067f8d7
Vaden Masrani <vadmas@cs.ubc.ca>
2020-10-27 17:05:58 -0700

    added pgroupby

--- flavor.py
+++ flavor.py
@@ -1,4 +1,5 @@
-from parallel import pmap_df
+import pandas as pd
+from parallel import pmap, pmap_df
 import pandas_flavor as pf
 import numpy as np
 
@@ -23,51 +24,18 @@ def remove_boring(df):
 def ppipe(df, f, **kwargs):
     return pmap_df(f, df, **kwargs)
 
-# collapse_levels(sep='_')
-# @pf.register_dataframe_method
-# def flatten_cols(df):
-#     df.columns = ['_'.join(col).strip() for col in df.columns.values]
-#     return df
-
-
-# to be converted to flavor when I find myself needing them
-
-# def process_dictionary_column(df, column_name):
-#     if column_name in df.columns:
-#         return (df
-#                 .join(df[column_name].apply(pd.Series))
-#                 .drop(column_name, 1))
-#     else:
-#         return df
-
-
-# def process_tuple_column(df, column_name, output_column_names):
-#     if column_name in df.columns:
-#         return df.drop(column_name, 1).assign(**pd.DataFrame(df[column_name].values.tolist(), index=df.index))
-#     else:
-#         return df
-
-
-# def process_list_column(df, column_name, output_column_names):
-#     if column_name in df.columns:
-#         new = pd.DataFrame(df[column_name].values.tolist(), index=df.index, columns=output_column_names)
-#         old = df.drop(column_name, 1)
-#         return old.merge(new, left_index=True, right_index=True)
-#     else:
-#         return df
-
-
-# def show_uniques(df):
-#     for col in df:
-#         print(f'{col}: ', df[col].unique())
-
-
-# def highlight_best(df, col):
-#     best = df[col].max()
-#     return df.style.apply(lambda x: ['background: lightgreen' if (x[col] == best) else '' for i in x], axis=1)
-
-
-# def filter_uninteresting(df):
-#     df = df.dropna(1, how='all')
-#     return df[[i for i in df if len(set(df[i])) > 1]]
+@pf.register_dataframe_method
+def pgroupby(df, groups, f,  **kwargs):
+    '''# mirror groupby order (group then agg)
+    replace:
+        results = df.groupby(['col1','col2']).apply(f)
+    with:
+        results = df.pgroupby(['col1','col2'], f)
+    '''
+    # split into names and groups
+    names, df_split = zip(*[(n,g) for n,g in df.groupby(groups)])
+    # pmap groups
+    out = pmap(f, df_split, **kwargs)
+    # reassemble and return
+    return pd.concat([pd.concat({k: v}, names=groups) for k, v in zip(names, out)])
 
