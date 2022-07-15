039835edae50ad2d6cc2a6069ee7ac361cff275d
Vaden Masrani <vadmas@cs.ubc.ca>
2022-07-15 13:20:21 -0700

    added axis param to parallel and adjust_lightness func

--- ml_helpers.py
+++ ml_helpers.py
@@ -562,6 +562,17 @@ def make_sparse(sparse_mx, args):
     return torch.sparse.FloatTensor(indices, values, shape)
 
 
+def adjust_lightness(color, amount=0.5):
+    """ https://stackoverflow.com/questions/37765197/darken-or-lighten-a-color-in-matplotlib """
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2])
+
 def exponentiate_and_normalize(values, dim=0):
     """Exponentiates and normalizes a tensor.
 
@@ -812,6 +823,7 @@ def default_init(args):
     args.home_dir = str(Path(args.home_dir).absolute())
     pprint(args.__dict__)
     return args
+
 
 def join_path(*args):
     return str(Path("/".join(args))) # trick to remove multiple backslashes
